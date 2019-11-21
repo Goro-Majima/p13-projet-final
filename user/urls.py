5bedde7597b50d5935464c866a37c2fa3936f831
Jiomod <lymickael91@gmail.com>
2019-11-21 12:49:22 +0100

    up

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from user import views as user_views
@@ -18,4 +18,18 @@ urlpatterns = [
     path('certificate_recall/<club_id>', views.certificate_recall, name='certificate_recall'),
     path('mail_sent/<club_id>', views.mail_sent, name='mail_sent'),
     path('xls_completed/<club_id>', views.xls_completed, name='xls_completed'),
+    path('password-reset',
+        auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), 
+            name='password_reset'),
+    path('password-reset/done',
+        auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), 
+            name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='user/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), 
+            name='password_reset_complete'),
 ]
